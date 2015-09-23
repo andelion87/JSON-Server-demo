f3b1eba5f94f2329d34f41bb8791863153a4d80e
xiaojuan <xj.ren@yinnut.com>
2015-09-23 11:32:18 +0800

    videojs-hls

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JSON-Server-demo
 json-server learning
+
+https://github.com/typicode/json-server
---
+++ json/index.js
@@ -0,0 +1,10 @@
+
+http_proxy=http://10.8.0.1:7128
+rts = function() {
+  var data = { users: [] }
+  // Create 1000 users
+  for (var i = 0; i < 1000; i++) {
+    data.users.push({ id: i, name: 'user' + i })
+  }
+  return data
+}
